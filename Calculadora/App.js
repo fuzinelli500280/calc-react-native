f424be35b42dcf44086839d60911a44ca0b9a434
fuzinelli500280 <riancaiofuzinelli12@gmail.com>
2024-05-22 21:35:46 -0300

    Introdução à Criação da Calculadora

---
+++ Calculadora/App.js
@@ -0,0 +1,19 @@
+import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import Botao from "./src/components/Botao";
+
+export default props => {
+  return (
+    <SafeAreaView style={estilos.container}>
+      <Botao />
+    </SafeAreaView>
+  )
+}
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5fcff",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
