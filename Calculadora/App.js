f74bba70a03a40a2629dbbbc51f6c581f261ea58
Rian Fuzinelli <144074554+fuzinelli500280@users.noreply.github.com>
2024-05-23 22:21:25 -0300

    Aplicação de Botões e Estilizações

--- Calculadora/App.js
+++ Calculadora/App.js
@@ -1,10 +1,33 @@
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import Botao from "./src/components/Botao";
 
 export default props => {
   return (
     <SafeAreaView style={estilos.container}>
-      <Botao />
+      <View style={estilos.botao}>
+
+      <Botao label="AC" triplo />
+      <Botao label="/" colorido />
+      <Botao label="7"/>
+      <Botao label="8"/>
+      <Botao label="9"/>
+      <Botao label="*"colorido />
+      
+      <Botao label="4"/>
+      <Botao label="5"/>
+      <Botao label="6"/>
+      <Botao label="-" colorido />
+
+      <Botao label="1"/>
+      <Botao label="2"/>
+      <Botao label="3"/>
+      <Botao label="+" colorido />
+
+      <Botao label="0" duplo />
+      <Botao label="."/>
+      <Botao label="=" colorido />
+
+      </View>
     </SafeAreaView>
   )
 }
@@ -12,8 +35,11 @@ export default props => {
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5fcff",
-    justifyContent: "center",
-    alignItems: "center"
-  }
+    backgroundColor: "#808080",
+    justifyContent: "flex-end"
+  },
+  botao: {
+    flexDirection: "row",
+    flexWrap: "wrap"
+  },
 })
